36e09127045c57ea3f1cf0e5344aafeb68ca63d8
asm-saim <saim777m@gmail.com>
2026-07-12 18:53:40 +0600

    from created with onSubmit handler and found output using e.target

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const SimpleForm = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+    }
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder='Enter Name' />
+                <br />
+                <input type="email" name="email" placeholder='Email' id="" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
