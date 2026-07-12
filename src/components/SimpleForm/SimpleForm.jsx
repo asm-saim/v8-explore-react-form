0d0ff60ab799463715ba7e2819e6fceac1fdc51d
asm-saim <saim777m@gmail.com>
2026-07-12 17:19:48 +0600

    file setup completed

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
