d9fcb1983ec702d1add24dc7399202e8d32b006f
asm-saim <saim777m@gmail.com>
2026-07-13 22:53:19 +0600

    context api applied on brother section

--- src/components/FamilyTree/Brother.jsx
+++ src/components/FamilyTree/Brother.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { MoneyContext } from './FamilyTree';
+import { use } from 'react';
 
 const Brother = () => {
+    const [money, setMoney] = use(MoneyContext)
+
     return (
         <div>
             <h3>Brother</h3>
+            <button onClick={() => setMoney(money + 1000)}>Add Money 1000</button>
         </div>
     );
 };
--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Grandpa from './Grandpa';
 import "./FamilyTree.css"
 import { createContext } from 'react';
@@ -6,16 +6,24 @@ import { createContext } from 'react';
 //Context API declaration: 
 export const AssetContext = createContext('')
 
+export const MoneyContext = createContext('')
+
 const FamilyTree = () => {
+
+    const [money, setMoney] = useState(0)
 
     const asset = "Diamond";
     const treasure = "lord of the ring"
     return (
         <div className='family'>
             <h2>Family Tree</h2>
-            <AssetContext.Provider value={treasure}>
-                <Grandpa asset={asset}></Grandpa>
-            </AssetContext.Provider>
+            <h3>Total family money: {money} </h3>
+            <MoneyContext value={[money, setMoney]}>
+                <AssetContext.Provider value={treasure}>
+                    <Grandpa asset={asset}></Grandpa>
+                </AssetContext.Provider>
+            </MoneyContext>
+
         </div>
     );
 };
