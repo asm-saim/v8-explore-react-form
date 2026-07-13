9311d8c1b505082601ab83d17d064630fa2c6254
asm-saim <saim777m@gmail.com>
2026-07-13 20:49:28 +0600

    context api implemented

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Grandpa from './Grandpa';
 import "./FamilyTree.css"
+import { createContext } from 'react';
+
+//Context API declaration: 
+export const AssetContext = createContext('')
 
 const FamilyTree = () => {
 
     const asset = "Diamond";
+    const treasure = "lord of the ring"
     return (
         <div className='family'>
             <h2>Family Tree</h2>
-            <Grandpa asset={asset}></Grandpa>
+            <AssetContext.Provider value={treasure}>
+                <Grandpa asset={asset}></Grandpa>
+            </AssetContext.Provider>
         </div>
     );
 };
--- src/components/FamilyTree/Friend.jsx
+++ src/components/FamilyTree/Friend.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
-const Friend = ({asset}) => {
+const Friend = ({ asset }) => {
+    // using context API
+    const newAsset = useContext(AssetContext)
+    console.log("NewAsset", newAsset)
     return (
         <div>
             <h3>Friend</h3>
             <p>Asset: {asset}</p>
+            <p>New Treasure:{newAsset}</p>
         </div>
     );
 };
